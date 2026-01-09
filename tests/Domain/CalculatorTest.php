a62537d08f3f0101eb7fd377fefb5323b4ed5219
Pokar Petr <pokar.petr@gmail.com>
2026-01-09 19:22:16 +0100

    Refactor CalculatorTest

--- tests/Domain/CalculatorTest.php
+++ tests/Domain/CalculatorTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-// require_once __DIR__ . '/../../includes/domain/class-calculator.php';
 
 final class CalculatorTest extends TestCase {
 
